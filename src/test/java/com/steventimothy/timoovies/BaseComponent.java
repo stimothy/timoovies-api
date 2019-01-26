65cd5e01624bc71220127d71f4a7b1107f6effd0
Steven Timothy <steven.timothy.cp@gmail.com>
2019-01-25 22:07:11 -0700

    TG-28 update unit tests

--- src/test/java/com/steventimothy/timoovies/BaseComponent.java
+++ src/test/java/com/steventimothy/timoovies/BaseComponent.java
@@ -1,12 +1,16 @@
 package com.steventimothy.timoovies;
 
+import com.steventimothy.timoovies.schema.User;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,6 +23,21 @@ public abstract class BaseComponent {
    */
   @Autowired
   protected TestRestTemplate restTemplate;
+
+  /**
+   * Creates a user in the database.
+   * @param id The id of the user.
+   * @param username The username of the user.
+   * @param password The password of the user.
+   * @return The response of the post to the create user endpoint.
+   */
+  protected ResponseEntity<Integer> createUser(Integer id, String username, String password) {
+    return this.restTemplate.exchange(RequestEntity.post(UriComponentsBuilder.fromUriString(getAmsPath())
+        .build().toUri())
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(new User().id(id).username(username).password(password)), Integer.class);
+  }
 
   /**
    * Asserts that the response status on the response is equal to the httpStatus provided.
--- src/test/java/com/steventimothy/timoovies/ams/controllers/AccountManagementControllerTest.java
+++ src/test/java/com/steventimothy/timoovies/ams/controllers/AccountManagementControllerTest.java
@@ -1,28 +1,20 @@
 package com.steventimothy.timoovies.ams.controllers;
 
 import com.steventimothy.timoovies.BaseComponent;
-import com.steventimothy.timoovies.schema.User;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AccountManagementControllerTest extends BaseComponent {
 
   /**
-   * This tests the ams health endpoint.
+   * This tests that a user can be created.
    */
   @Test
   public void testCreateUser() {
-    ResponseEntity<Integer> responseEntity = this.restTemplate.exchange(RequestEntity.post(UriComponentsBuilder.fromUriString(getAmsPath())
-        .build().toUri())
-        .accept(MediaType.APPLICATION_JSON)
-        .contentType(MediaType.APPLICATION_JSON)
-        .body(new User().id(1).username("bob1234").password("myPassword")), Integer.class);
+    ResponseEntity<Integer> responseEntity = createUser(1, "bob1234", "myPassword");
 
     assertStatus(responseEntity, HttpStatus.OK);
     assertThat(responseEntity.getBody())
